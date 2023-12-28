9ed6a549fbbd2fd26f58cc8672fc62649b8b651d
tkhabc <tkianhon@gmail.com>
2023-12-29 04:16:34 +0800

    .

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
-<script>
+<!-- <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
@@ -12,7 +18,7 @@ export default {
     HelloWorld
   }
 }
-</script>
+</script> -->
 
 <style>
 #app {
@@ -23,4 +29,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
 </style>

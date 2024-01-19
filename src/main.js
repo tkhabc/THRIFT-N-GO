f51b93f34bcc5391b48d3ab6324373de2b9168f3
tkhabc <tkianhon@gmail.com>
2024-01-19 22:59:36 +0800

    delete css

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import {store} from './store'
 import Vuex from 'vuex'
-//import '../src/input.css'
+
 
 createApp(App)
     .use(store)
     .use(router)
     .use(Vuex)
     .mount('#app')
+ 

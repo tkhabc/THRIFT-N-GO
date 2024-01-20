59f3f8dab9b55b1f992db9ffbf6b818b6b5499fe
tkhabc <tkianhon@gmail.com>
2024-01-21 00:33:51 +0800

    sidebar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,58 +1,39 @@
-<template>
-  
-  <!-- <v-card max-width="448" class="mx-auto" color="grey-lighten-3">
-      <v-layout> -->
-        
-        <v-app-bar
-          color="teal-darken-4"
-          image="https://picsum.photos/1920/1080?random"
-        >
-          <template v-slot:image>
-            <v-img
-              gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-            ></v-img>
-          </template>
+<template>       
+  <v-app-bar
+    color="teal-darken-4"
+    image="https://picsum.photos/1920/1080?random"
+  >
+    <template v-slot:image>
+      <v-img
+        gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+      ></v-img>
+    </template>
 
-          <template v-slot:prepend>
-            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-          </template>
+    <template v-slot:prepend>
+      <v-img hover="click me" :src="require('@/assets/ThriftNGoLogo.png')" width="60" alt="Thrift N Go logo" style="cursor: pointer" @click="navigateToHome" />
+    </template>
 
-          <v-app-bar-title>Thrift N Go</v-app-bar-title>
+    <template v-slot:append>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </template>    
+    
+    <v-app-bar-title>THRIFT N GO</v-app-bar-title>
 
-          
+    <v-spacer></v-spacer>
+  </v-app-bar>
 
-          <v-spacer></v-spacer>
+  <v-navigation-drawer location="right" width="200" v-model="drawer">
+      <v-list>
+        <v-list-item v-for="item in menuItems" :key="item.title" @click="drawer = false">
+          <v-list-item-content>
+            <v-list-item-title>
+              <router-link :to="item.path">{{ item.title }}</router-link>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-
-          <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-btn>
-
-          <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </v-app-bar>
-        
-
-        <v-navigation-drawer v-model="drawer">
-            <v-list>
-              <v-list-item v-for="item in menuItems" :key="item.title" @click="drawer = false">
-                <v-list-item-content>
-                  <v-list-item-title>
-                    <router-link :to="item.path">{{ item.title }}</router-link>
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-navigation-drawer>
-
-          
-
-      <!-- </v-layout>
-  </v-card> -->
 </template>
 
 <script>
@@ -71,5 +52,10 @@ export default {
       { title: 'Profile', path: '/profile' },
     ]
   }),
+  methods: {
+    navigateToHome() {
+      this.$router.push('/');
+    }
+  }
 };
 </script>

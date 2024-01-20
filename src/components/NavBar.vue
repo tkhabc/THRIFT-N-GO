76988e140ad63553b942dcbdc1ef8ee93a042b96
tkhabc <tkianhon@gmail.com>
2024-01-20 23:07:56 +0800

    add NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,9 @@
 <template>
   
-  <v-card max-width="448" class="mx-auto" color="grey-lighten-3">
-      <v-layout>
-        <!-- <v-app> -->
-
-        <!-- <v-app-bar
+  <!-- <v-card max-width="448" class="mx-auto" color="grey-lighten-3">
+      <v-layout> -->
+        
+        <v-app-bar
           color="teal-darken-4"
           image="https://picsum.photos/1920/1080?random"
         >
@@ -20,19 +19,7 @@
 
           <v-app-bar-title>Thrift N Go</v-app-bar-title>
 
-          <v-navigation-drawer v-model="drawer">
-            <v-list>
-              <v-list-item v-for="item in menuItems" :key="item.title" @click="drawer = false">
-                <v-list-item-content>
-                  <v-list-item-title>
-                    <router-link :to="item.path">{{ item.title }}</router-link>
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-navigation-drawer>
-
-           <router-view></router-view> 
+          
 
           <v-spacer></v-spacer>
 
@@ -47,28 +34,42 @@
           <v-btn icon>
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
-        </v-app-bar> -->
+        </v-app-bar>
+        
 
-        <!-- </v-app> -->
-      </v-layout>
-  </v-card>
+        <v-navigation-drawer v-model="drawer">
+            <v-list>
+              <v-list-item v-for="item in menuItems" :key="item.title" @click="drawer = false">
+                <v-list-item-content>
+                  <v-list-item-title>
+                    <router-link :to="item.path">{{ item.title }}</router-link>
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-navigation-drawer>
+
+          
+
+      <!-- </v-layout>
+  </v-card> -->
 </template>
 
 <script>
 export default {
-  // data: () => ({
-  //   drawer: false,
-  //   menuItems: [
-  //     { title: 'Register', path: '/register' },
-  //     { title: 'Login', path: '/login' },
-  //     { title: 'Home', path: '/' },
-  //     { title: 'About Us', path: '/about' },
-  //     { title: 'Food Listing', path: '/foodlisting' },
-  //     { title: 'Item Listing', path: '/itemlisting' },
-  //     { title: 'Cart', path: '/cart' },
-  //     { title: 'Contact', path: '/contact' },
-  //     { title: 'Profile', path: '/profile' },
-  //   ]
-  // }),
+  data: () => ({
+    drawer: false,
+    menuItems: [
+      { title: 'Register', path: '/register' },
+      { title: 'Login', path: '/login' },
+      { title: 'Home', path: '/' },
+      { title: 'About Us', path: '/about' },
+      { title: 'Food Listing', path: '/foodlisting' },
+      { title: 'Item Listing', path: '/itemlisting' },
+      { title: 'Cart', path: '/cart' },
+      { title: 'Contact', path: '/contact' },
+      { title: 'Profile', path: '/profile' },
+    ]
+  }),
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,13 @@
 <template>
     <div class="home">
       <img alt="Thrift N Go logo" width="100%" src="../assets/food.jpg">
-      <!-- <HelloWorld msg="Welcome to Thrift N Go"/> -->
     </div>
 
-    <img alt="Thrift N Go Logo" src="../assets/ThriftNGoLogo.png">
+    <img alt="Thrift N Go Logo" width="100%" src="../assets/ThriftNGoLogo.png">
 
   </template>
+
 
   <script>
   
   </script>
-
-<!-- import HelloWorld from '@/components/HelloWorld.vue'
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-} -->
